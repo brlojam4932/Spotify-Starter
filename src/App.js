bb283cd6bd301acf1d89bd97804b0be85bef0d3c
brlojam4932 <36285465+brlojam4932@users.noreply.github.com>
2022-05-10 20:01:12 -0700

    re-do
    I started project from start

--- src/App.js
+++ src/App.js
@@ -1,16 +1,57 @@
-import React from 'react';
-import { Routes, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Album from './pages/Album';
 import './App.css';
+import { Layout } from 'antd';
+import Spotify from "./images/Spotify.png";
+import { SearchOutlined, DownCircleOutlined } from '@ant-design/icons';
+import AudioPlayer from './components/AudioPlayer';
+
+const { Header, Footer, Sider, Content } = Layout;
+
 
 const App = () => {
-  return(
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/album" element={<Album />} />
-    </Routes>
-  )
+  const [nftAlbum, setNftAlbum] = useState();
+
+  return (
+    <Layout>
+      <Layout>
+        <Sider width={300} className="sideBar">
+          <img src={Spotify} alt="logo" className="logo"></img>
+          <div className="searchBar">
+            <span>Search</span>
+            <SearchOutlined style={{ fontSize: "30px" }} />
+          </div>
+          <Link to="/">
+            <p style={{ color: "#108954" }}>Home</p>
+          </Link>
+          <p>Your Music</p>
+          <div className="recentPlayed">
+            <p className="recentTitle">RECENTLY PLAYED</p>
+            <div className="install">
+              <span>Install App</span>
+              <DownCircleOutlined style={{ fontSize: "38px" }} />
+            </div>
+          </div>
+        </Sider>
+        <Content className="contentWindow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/album" element={<Album setNftAlbum={setNftAlbum} />} />
+          </Routes>
+        </Content>
+      </Layout>
+      <Footer className="footer">
+        {nftAlbum &&
+          <AudioPlayer nftAlbum={nftAlbum} />
+        }
+      </Footer>
+    </Layout>
+  );
 };
 
 export default App;
+
+// yarn install
+// yarn start
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,123 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import "./Home.css";
+import { Tabs } from 'antd';
+import { library } from '../helpers/albumList';
+
+const { TabPane } = Tabs;
 
 const Home = () => {
 
-return(
-  <>
-  <div className="container">
-     <Link to="/album" className="link">Album Page</Link>
-  </div>
-  </>
-)
+  return (
+    <>
+      <Tabs defaultActiveKey="1" centered>
+        <TabPane tab="FEATURED" key="1">
+          <h1 className="featuredTitle">Today Is The Day</h1>
+          <div className="albums">
+            {library.map((libraries) => (
+              <Link to="/album" state={libraries} className="albumSelection">
+                <img
+                  src={libraries.image}
+                  alt="bull"
+                  style={{ width: "150px", marginBottom: "10px" }}
+                >
+                </img>
+                <p>{libraries.title}</p>
+              </Link>
+            ))}
+          </div>
+        </TabPane>
+        <TabPane tab="GENRES" key="2">
+          <h1 className="featuredTitle">Top Hits</h1>
+          <div className="albums">
+            {library.slice(7, 13).map((libraries) => (
+              <Link to="/album" state={libraries} className="albumSelection">
+                <img
+                  src={libraries.image}
+                  alt="bull"
+                  style={{ width: "150px", marginBottom: "10px" }}
+                >
+                </img>
+                <p>{libraries.title}</p>
+              </Link>
+            ))}
+          </div>
+          <h1 className="featuredTitle">Electronic</h1>
+          <div className="albums">
+            {library.slice(0, 6).map((libraries) => (
+              <Link to="/album" state={libraries} className="albumSelection">
+                <img
+                  src={libraries.image}
+                  alt="bull"
+                  style={{ width: "150px", marginBottom: "10px" }}
+                >
+                </img>
+                <p>{libraries.title}</p>
+              </Link>
+            ))}
+          </div>
+          <h1 className="featuredTitle">Hip Hop</h1>
+          <div className="albums">
+            {library.slice(2, 8).map((libraries) => (
+              <Link to="/album" state={libraries} className="albumSelection">
+                <img
+                  src={libraries.image}
+                  alt="bull"
+                  style={{ width: "150px", marginBottom: "10px" }}
+                >
+                </img>
+                <p>{libraries.title}</p>
+              </Link>
+            ))}
+          </div>
+          <h1 className="featuredTitle">Classical</h1>
+          <div className="albums">
+            {library.slice(8, 18).map((libraries) => (
+              <Link to="/album" state={libraries} className="albumSelection">
+                <img
+                  src={libraries.image}
+                  alt="bull"
+                  style={{ width: "150px", marginBottom: "10px" }}
+                >
+                </img>
+                <p>{libraries.title}</p>
+              </Link>
+            ))}
+          </div>
+          <h1 className="featuredTitle">Country</h1>
+          <div className="albums">
+            {library.slice(9, 16).map((libraries) => (
+              <Link to="/album" state={libraries} className="albumSelection">
+                <img
+                  src={libraries.image}
+                  alt="bull"
+                  style={{ width: "150px", marginBottom: "10px" }}
+                >
+                </img>
+                <p>{libraries.title}</p>
+              </Link>
+            ))}
+          </div>
+        </TabPane>
+        <TabPane tab="NEW RELEASES" key="3">
+          <h1 className="featuredTitle">Today Is The Day</h1>
+          <div className="albums">
+            {library.map((libraries) => (
+              <Link to="/album" state={libraries} className="albumSelection">
+                <img
+                  src={libraries.image}
+                  alt="bull"
+                  style={{ width: "150px", marginBottom: "10px" }}
+                >
+                </img>
+                <p>{libraries.title}</p>
+              </Link>
+            ))}
+          </div>
+        </TabPane>
+      </Tabs>
+    </>
+  )
 }
 
 export default Home;
